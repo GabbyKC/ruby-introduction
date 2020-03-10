7f7b75d5045ac38d47c8a38778217ed2f166afe6
Gabriella Cannon <gabriella.cannon@TwentyBNs-MacBook-Pro-2.local>
2020-03-10 13:34:49 +0100

    first commit

---
+++ playground.rb
@@ -0,0 +1,8 @@
+# https://www.rubyguides.com/2018/10/puts-vs-print/
+puts "Hello World"
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+p numbers
+p numbers[4]
+new_numbers = numbers.reverse!
+p new_numbers
